0e2fed23886634a897e3220bbd303eda34c87980
MKello <webturing256@163.com>
2024-01-27 22:54:06 +0800

    添加Evictor缓存定时驱逐组件测试用例（这个组件很好，只是提示自己别忘记）

--- src/main/java/org/example/FreeMarkerDemoApplication.java
+++ src/main/java/org/example/FreeMarkerDemoApplication.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.stoyanr.evictor.map.ConcurrentHashMapWithTimedEviction;
+import com.stoyanr.evictor.scheduler.DelayedTaskEvictionScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreeMarkerDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(FreeMarkerDemoApplication.class, args);
+
+        // 测试evictor缓存定时驱逐组件 -> https://github.com/MagicFollower/Evictor
+        ConcurrentHashMapWithTimedEviction<Object, Object> mapWithTimedEviction =
+                new ConcurrentHashMapWithTimedEviction<>(256, new DelayedTaskEvictionScheduler<>());
+        mapWithTimedEviction.put("A", "1");
+        mapWithTimedEviction.put("A", "2");
+        mapWithTimedEviction.put("B", "1");
+        mapWithTimedEviction.putIfAbsent("B", "2");
+        System.out.println("mapWithTimedEviction = " + mapWithTimedEviction);
     }
 }
